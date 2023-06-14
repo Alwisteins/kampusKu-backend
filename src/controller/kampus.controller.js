0547878da61be8f4a3ae60d97e2b74692f30489c
unknown <alwyyourbae@gmail.com>
2023-06-13 17:44:55 -0700

    feat(kampus.controller.js): add getCampusByTipe, getCampusByRank, getCampusByFakultas, dll

--- src/controller/kampus.controller.js
+++ src/controller/kampus.controller.js
@@ -70,10 +70,106 @@ const getCampusByName = catchAsync(async (req, res) => {
   });
 });
 
+const getCampusByTipe = catchAsync(async (req, res) => {
+  const { tipe } = req.body;
+
+  // 1) catch all return from kampusService into variabel
+  const {
+    statusCode, status, message, kampus,
+  } = await kampusService.getCampusByTipe(tipe);
+
+  // 2) validate if status variabel value false
+  if (status === false) {
+    return res.status(statusCode).json({
+      status,
+      message,
+    });
+  }
+
+  // 3) response if success
+  return res.status(statusCode).json({
+    status,
+    message,
+    kampus,
+  });
+});
+
+const getCampusByRank = catchAsync(async (req, res) => {
+  // 1) catch all return from kampusService into variabel
+  const {
+    statusCode, status, message, kampus,
+  } = await kampusService.getCampusByRank();
+
+  // 2) validate if status variabel value false
+  if (status === false) {
+    return res.status(statusCode).json({
+      status,
+      message,
+    });
+  }
+
+  // 3) response if success
+  return res.status(statusCode).json({
+    status,
+    message,
+    kampus,
+  });
+});
+
+const getCampusByFakultas = catchAsync(async (req, res) => {
+  const { fakultas } = req.body;
+  // 1) catch all return from kampusService into variabel
+  const {
+    statusCode, status, message, kampus,
+  } = await kampusService.getCampusByFakultas(fakultas);
+
+  // 2) validate if status variabel value false
+  if (status === false) {
+    return res.status(statusCode).json({
+      status,
+      message,
+    });
+  }
+
+  // 3) response if success
+  return res.status(statusCode).json({
+    status,
+    message,
+    kampus,
+  });
+});
+
+const getCampusByAkreditasi = catchAsync(async (req, res) => {
+  const { rank } = req.body;
+  // 1) catch all return from kampusService into variabel
+  const {
+    statusCode, status, message, kampus,
+  } = await kampusService.getCampusByRank(rank);
+
+  // 2) validate if status variabel value false
+  if (status === false) {
+    return res.status(statusCode).json({
+      status,
+      message,
+    });
+  }
+
+  // 3) response if success
+  return res.status(statusCode).json({
+    status,
+    message,
+    kampus,
+  });
+});
+
 const kampusController = {
   getAllCampus,
   getCampusById,
   getCampusByName,
+  getCampusByTipe,
+  getCampusByRank,
+  getCampusByFakultas,
+  getCampusByAkreditasi,
 };
 
 export default kampusController;
