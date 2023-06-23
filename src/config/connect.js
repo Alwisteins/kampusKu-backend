d39c9eedb4d85fae67fb412ff94b986af1d7ca95
unknown <alwyyourbae@gmail.com>
2023-06-23 08:08:58 -0700

    feat: add database url(dont worry its read only access)

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,6 +1,9 @@
 import mysql from "mysql2";
 
 export const init = () => {
-  mysql.createPool(process.env.DATABASE_URL);
+  mysql.createPool(
+    process.env.READ_DATABASE_URL
+      || "mysql://5n6xbrioptgu8d828ih4:pscale_pw_5NyWBOhAxv8nTaZRPTPyJDqSk0DsUky4IlQZTKUlTSF@aws.connect.psdb.cloud/kampusku_database?ssl={\"rejectUnauthorized\":true}",
+  );
   console.log("connected with database!");
 };
